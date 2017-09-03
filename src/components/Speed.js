703313e6a6473a4b527d1f1babd0cb988fae4491
Avery Biskup <avery@Averys-MacBook-Pro.local>
2017-09-02 20:37:08 -0700

    Removed input box

--- src/components/Speed.js
+++ src/components/Speed.js
@@ -11,16 +11,6 @@ export class Speed extends Component {
     num = num / 60.00
     this.props.cb(parseFloat(1000.00 / num))
   }
-  sendData = () => {
-    let temp = document.getElementById("speed-input").value.replace(/\D+/g, '')
-    if (temp.length <= 0) {
-      alert("Invalid input")
-    } else {
-      this.calculate(parseFloat(temp))
-    }
-    document.getElementById("speed-input").className = "in"
-    console.log('hello')
-  }
   changeValue = () => {
     let sliderNum = document.getElementById("range").value
     this.calculate(sliderNum)
@@ -32,16 +22,6 @@ export class Speed extends Component {
       <div>
         <div id="slider-output">WPM: 60</div>
         <div id="input-range"><input id="range" type="range" max="600" min="60" step="5" onChange={this.changeValue}/></div>
-        <div id="speed-container">
-          <input id="speed-input" type="text" onBlur={() => { if (document.getElementById("speed-input").value.length > 0) this.sendData() }} placeholder="New WPM" onKeyPress={(e) => {
-            if (!e) e = window.event
-            var keyCode = e.keyCode || e.which
-            if (keyCode == '13') {
-              this.sendData()
-              return false
-            }
-          }} />
-        </div>
       </div>
     )
   }
