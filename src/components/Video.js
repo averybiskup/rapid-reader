7d1c5b6e82a9cb66cf03a8233134160df0c00823
Avery Biskup <avery@Averys-MacBook-Pro.local>
2017-09-09 14:52:59 -0700

    Embeded music

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './../stylesheets/index.css'
 const { Component } = React
 
 export class Video extends Component {
   render() {
     return (
       <div>
-      <iframe width="420" height="345" src="https://www.youtube.com/embed/J---aiyznGQ"></iframe>
+      <iframe id="vid" width="420" height="345" src="https://www.youtube.com/embed/J---aiyznGQ?autoplay=1"></iframe>
       </div>
     )
   }
